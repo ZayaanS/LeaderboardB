27428c45c1b35d6b444d7ca557f82d9816724603
zayaans <zayaandots@gmail.com>
2021-05-24 11:30:28 +0200

    added unit tests for model

--- website/models.py
+++ website/models.py
@@ -6,31 +6,31 @@ from sqlalchemy.sql import func
 
 # notes table
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True) # db will add this
+    data = db.Column(db.String(10000)) # user input
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # db will add this
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # current_user will add this and db will pick up the relationship
 
 # work table
 class Work(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True) # db will create
+    title = db.Column(db.String(200)) 
     description = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    status = db.Column(db.String(100))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # db will create
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    status = db.Column(db.String(100)) 
     points = db.Column(db.Integer)
 
 # user table
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) # created by db
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(255))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note') # store all notes user has
+    notes = db.relationship('Note') # store all notes user has, based on db relationship
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
     team_leader = db.Column(db.Boolean)
-    work = db.relationship('Work') # store all work user has
+    work = db.relationship('Work') # store all work user has, based on db relationship
     points = db.Column(db.Integer)
 
 # teams table
